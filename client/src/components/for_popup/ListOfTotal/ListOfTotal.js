acbc7693d3de51d31b9f66f73c1fe6527aca7a62
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-07-28 00:59:07 +0600

    login page done

--- client/src/components/for_popup/ListOfTotal/ListOfTotal.js
+++ client/src/components/for_popup/ListOfTotal/ListOfTotal.js
@@ -4,7 +4,7 @@ const ListOfTotal = () => {
 	return (
 		<>
 			<div className="container-fluid p-0 total-main-container">
-				<div className="row m-0">
+				<div className="row m-0 total-wrapper">
 					<div className="col-9 p-0 total-container"></div>
 				</div>
 			</div>
