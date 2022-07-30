70800e0a3adb6076d05a5efc2d55483be9eda21e
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-07-30 21:15:09 +0600

    user-box attach done

--- client/src/components/for_homepage/GroupChat/GroupChat.js
+++ client/src/components/for_homepage/GroupChat/GroupChat.js
@@ -1,7 +1,20 @@
+// internal components
 import "./GroupChat.css";
+import UserBox from "./UserBox/UserBox";
 
 const GroupChat = () => {
-	return <div>GroupChat</div>;
+	return (
+		<>
+			<div className="group-chat-container">
+				<div className="row">
+					<div className="col-5">
+						<UserBox />
+					</div>
+					<div className="col-7"></div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default GroupChat;
--- client/src/components/for_homepage/GroupChat/UserBox/UserBox.js
+++ client/src/components/for_homepage/GroupChat/UserBox/UserBox.js
@@ -1,46 +1,48 @@
 // external components
 
 // internal components
-import "./Middle.css";
 import { d_user } from "../../../../dummy_data";
+import "./UserBox.css";
 
-const Middle = () => {
+const UserBox = () => {
 	return (
 		<>
-			<div className="search">
-				<i className="bi bi-search-heart"></i>
-				<input
-					type="search"
-					name="search"
-					id="search"
-					autoComplete="off"
-					placeholder="Search or start new chat"
-				/>
+			<div className="user-box">
+				<div className="search">
+					<i className="bi bi-search-heart"></i>
+					<input
+						type="search"
+						name="search"
+						id="search"
+						autoComplete="off"
+						placeholder="Search or start new chat"
+					/>
+				</div>
+
+				{d_user &&
+					d_user.map((value, index) => {
+						return (
+							<div className="user" key={index}>
+								<img
+									src={value.img}
+									alt="profile-img"
+									className="profile-img img-fluid"
+								/>
+
+								<section>
+									<div className="above">
+										<h6>{value.name}</h6>
+										<span>{value.time}</span>
+									</div>
+
+									<div className="down">{value.last_message}</div>
+								</section>
+							</div>
+						);
+					})}
 			</div>
-
-			{d_user &&
-				d_user.map((value, index) => {
-					return (
-						<div className="user" key={index}>
-							<img
-								src={value.img}
-								alt="profile-img"
-								className="profile-img img-fluid"
-							/>
-
-							<section>
-								<div className="above">
-									<h6>{value.name}</h6>
-									<span>{value.time}</span>
-								</div>
-
-								<div className="down">{value.last_message}</div>
-							</section>
-						</div>
-					);
-				})}
 		</>
 	);
 };
 
-export default Middle;
+export default UserBox;
